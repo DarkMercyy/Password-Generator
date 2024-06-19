08d633039ae8fa11180e48194a7a25974205134c
Xeozus <112655499+DarkMercyy@users.noreply.github.com>
2024-06-19 10:46:32 +0200

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -2,22 +2,16 @@ import string
 import random
 import time
 
-
-
-
 print("Hello! Here are your passwords sir:")
 
-
-
 def generate_password(length):
-  
-      all_characters = string.ascii_letters + string.digits + string.punctuation
-
-      return ''.join(random.choice(all_characters) for i in range(length))
+    safe_special_characters = "!#$%&*+-=?@^_"
+    all_characters = string.ascii_letters + string.digits + safe_special_characters
+    return ''.join(random.choice(all_characters) for i in range(length))
 
 for i in range(20):
-  time.sleep(0.3)
-  password = generate_password(20)
-  print(password)
+    time.sleep(0.3)
+    password = generate_password(20)
+    print(password)
 
 input("Press Enter to exit...")
